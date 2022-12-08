e6c020f5828dd56f4c4b35d77d075097a9c7006c
jmahalin <81984285+jmahalin@users.noreply.github.com>
2022-12-08 11:12:00 -0500

    Update script.sh
    hard-code namespace to get workflow working

--- .github/script.sh
+++ .github/script.sh
@@ -4,6 +4,9 @@ app_name=$1
 image=$2
 namespace=$3
 
+# hard-coded to get workflow working
+namespace="jmahalin-dev"
+
 deploy=`oc get deployment $app_name -n $namespace`
 if [[ "$?" -eq 0 ]]; then
     oc set image deployment/$app_name $app_name=$image -n $namespace
